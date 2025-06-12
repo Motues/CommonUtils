a24d30e34774e1d007ebcea4cb70c43d83168003
Motues <2901987286@qq.com>
2025-06-12 13:56:47 +0800

    添加 JSON 模块
    - 新增 JSON 模块，基于 nlohmann/json 实现链式访问、类型安全获取等特性
    - 添加 JSON 模块的 README.md 和测试文件，说明使用方法和功能特性

---
+++ JSON/test/Test.cpp
@@ -0,0 +1,18 @@
+#include "Utils/JSON.hpp"
+#include <iostream>
+
+int main() {
+    using namespace Utils::JSON;
+
+    JsonManager json("test.json");
+
+    std::cout << json["name"].String() << std::endl;
+    std::cout << json["version"].String() << std::endl;
+    std::cout << json["array"][0].Int()  << std::endl;
+    std::cout << json["object"]["a"].Int()  << std::endl;
+
+    json["array"][0].set(20);
+    json.saveToFile("test_new.json");
+
+    return 0;
+}
